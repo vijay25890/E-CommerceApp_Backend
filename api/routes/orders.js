5e3d2ae3a5a589c8a4134679a5486e2504460180
vijay25890 <vijaymore25890@gmail.com>
2021-09-15 19:15:31 +0530

    order route completed

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,125 +8,83 @@ const checkAuth = require("../middleware/check_auth");
 /*
 Route           /
 Description     get all orders
-Access          PUBLIC
+Access          PRIVATE
 Parameters      NONE
 Method          GET
 */
-router.get("/",checkAuth, (req, res, next) => {
-  Order.find()
-    .select("product quantity _id")
-    .exec()
-    .then((docs) => {
-      res.status(200).json({
-        count: docs.length,
-        orders: docs.map((docs) => {
-          return {
-            id: docs._id,
-            product: docs.product,
-            quantity: docs.quantity,
-          };
-        }),
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
+router.get("/", checkAuth, async (req, res, next) => {
+  try {
+    const getAllOrders = await Order.find().select("product quantity _id");
+    return res.status(200).json(getAllOrders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 /*
 Route           /
 Description     add orders
-Access          PUBLIC
+Access          PRIVATE
 Parameters      NONE
-Method          GET
+Method          POST 
 */
-router.post("/",checkAuth, (req, res, next) => {
-  Product.findById(req.body.productId)
-    .then((product) => {
-      if (!product) {
-        return res.status(404).json({
-          message: "Product not found",
-        });
-      }
-      const order = new Order({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId,
+router.post("/", checkAuth, async (req, res, next) => {
+  try {
+    const addNewOrder = await Product.findById(req.body.productId);
+    if (!addNewOrder) {
+      return res.status(404).json({
+        message: "Product not found",
       });
-      return order.save();
-    })
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Order stored",
-        createdOrder: {
-          _id: result._id,
-          product: result.product,
-          quantity: result.quantity,
-        },
-        request: {
-          type: "GET",
-          url: "http://localhost:3000/orders/" + result._id,
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
+    }
+    const order = await new Order({
+      _id: mongoose.Types.ObjectId(),
+      quantity: req.body.quantity,
+      product: req.body.productId,
     });
+    await Order.create(order);
+    res.status(500).json("Order Created");
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 /*
 Route           /
 Description     get specific order
-Access          PUBLIC
+Access          PRIVATE
 Parameters      :orderId
 Method          GET
 */
-router.get("/:orderId",checkAuth, (req, res, next) => {
-  Order.findById(req.params.orderId)
-    .exec()
-    .then((order) => {
-      if (!order) {
-        return res.status(404).json({
-          message: "Order not found",
-        });
-      }
-      res.status(200).json({
-        order: order,
+router.get("/:orderId", checkAuth, async (req, res, next) => {
+  try {
+    const getSpecificOrder = await Order.findById(req.params.orderId);
+    if (!getSpecificOrder) {
+      return res.status(500).json({
+        message: `No order found at ${req.params.orderId}`,
       });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
+    }
+    return res.status(200).json(getSpecificOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 /*
 Route           /
 Description     delete order
-Access          PUBLIC
+Access          PRIVATE
 Parameters      :orderId
-Method          GET
+Method          DELETE
 */
-router.delete("/:orderId",checkAuth, (req, res, next) => {
-  Order.remove({ _id: req.params.orderId })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Order deleted",
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
+router.delete("/:orderId", checkAuth, (req, res, next) => {
+  try {
+    const deleteOrder = Order.findOneAndDelete({ _id: req.params.orderId });
+    res.status(200).json({
+      message: "Order deleted",
     });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
