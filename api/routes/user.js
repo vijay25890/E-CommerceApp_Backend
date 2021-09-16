55eb514cb339ad70ec31a3f4561f41695c2a61c4
vijay25890 <vijaymore25890@gmail.com>
2021-09-16 12:37:59 +0530

    Auth added

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,105 +5,80 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
-const user = require("../models/user");
+const { token } = require("morgan");
 
-router.post("/signup", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          message: "Mail exists",
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err,
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash,
-            });
-            user
-              .save()
-              .then((result) => {
-                console.log(result);
-                res.status(201).json({
-                  message: "User created",
-                });
-              })
-              .catch((err) => {
-                console.log(err);
-                res.status(500).json({
-                  error: err,
-                });
-              });
-          }
-        });
-      }
+/*
+Route           /signup
+Description     create user
+Access          PUBLIC
+Parameter       NONE
+Methods         POST
+*/
+router.post("/signup", async (req, res, next) => {
+  const { email, password: plainTextPassword } = req.body;
+  if (plainTextPassword.length < 5) {
+    return res.json("Please! create a secure password");
+  }
+  const password = await bcrypt.hash(plainTextPassword, 10);
+
+  try {
+    const user = await User.create({
+      email,
+      password,
     });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post("/login", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: "Auth failed",
-        });
+/*
+Route           /login
+Description     login user
+Access          PUBLIC
+Parameter       NONE
+Methods         POST
+*/
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).lean();
+  if (!email) {
+    return res.status(500).json("invalid username and paasword");
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id,
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: "1h",
       }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-          return res.status(401).json({
-            message: "Auth failed",
-          });
-        }
-        if (result) {
-          const token = jwt.sign(
-            {
-              email: user[0].email,
-              userId: user[0]._id,
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: "1h",
-            }
-          );
-          return res.status(200).json({
-            message: "Auth successful",
-            token: token,
-          });
-        }
-        res.status(401).json({
-          message: "Auth failed",
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
+    );
+  }
+  return res.status(200).json({
+    message: "Auth successful",
+    token: token,
+  });
 });
 
-router.delete("/:userId", (req, res, next) => {
-  User.remove({ _id: req.params.userId })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "User deleted",
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
+/*
+Route           /
+Description     delete user
+Access          PUBLIC
+Parameter       :userId
+Methods         DELETE
+*/
+router.delete("/:userId", async (req, res, next) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.userId });
+    res.status(200).json({
+      message: "User deleted sucessfully",
+      user: user,
     });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 module.exports = router;
